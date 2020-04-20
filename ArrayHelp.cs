a3d97af483e4268434b9dcaf88d0ea55e37b2c1b
shohincoding-com <63291350+shohincoding-com@users.noreply.github.com>
2020-04-21 03:16:24 +0500

    Обработка методи. Также создал класс для list коммнади. При помощю list создал список комманди. И тестировал первый метод для удаление элемент

--- ArrayHelp.cs
+++ ArrayHelp.cs
@@ -5,16 +5,33 @@ namespace HW_13_Array
     {
         public static S Pop(ref S[] Mass)
         {
-            
+            S FinalMass;
+            FinalMass = Mass[Mass.Length - 1];
+            S[] NewMass = new S[Mass.Length -1];
+            for(int j = 0; j < Mass.Length - 1; j++)
+            NewMass[j] = Mass[j];
+            Mass = NewMass;
+            return FinalMass;
         }
         public static int Push(ref S[] Mass, S NewElem)
         {
-            
+            S[] NewMass = new S[Mass.Length + 1];
+            for (int j = 0; j < Mass.Length; j++ )
+            NewMass[j] = Mass[j]; 
+            NewMass[Mass.Length] = NewElem;
+            Mass = NewMass;
+            return Mass.Length; 
         }
 
         public static S Shift(ref S[] Mass)
         {
-            
+            S FinalMass;
+            FinalMass = Mass[0];
+            S[] NewMass = new S[Mass.Length-1];
+            for ( int j = 1; j < Mass.Length; j++ )
+            NewMass[j - 1] = Mass[j]; 
+            Mass = NewMass;
+            return FinalMass;
         }
 
         public static int Unshift(ref S[] Mass, S NewElem)
--- Program.cs
+++ Program.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using HW_13_Array;
 
 namespace HW_13
 {
+    class UserCommand
+    {
+    public string Cmd { get; set; }
+
+    
+    }
     class Program
     {
         static void Main(string[] args)
         {
-                           
+            int [] IntMass = new int [] {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
+            List<UserCommand> listOfUsers = new List<UserCommand>()
+            {
+                new UserCommand() { Cmd ="1. Удалит последний элемент"},
+                new UserCommand() { Cmd ="1. Добавить элемент с конца"},
+                new UserCommand() { Cmd ="1. Удалит первий элемент"},
+                new UserCommand() { Cmd ="1. Добавить элемент в начало"},
+                new UserCommand() { Cmd ="1. Возвращат новый массив"},
+            };
+
+            for(int i = 0; i < listOfUsers.Count; i++)
+            {
+                Console.WriteLine(listOfUsers[i].Cmd );
+            }
+
+            int UserCmd = int.Parse(Console.ReadLine());
+            
+            if(UserCmd == 1)
+            {
+                Console.Write("Удалено слейдущий элемент: ");
+                int NewElem = ArrayHelp<int>.Pop(ref IntMass);
+                Console.WriteLine(NewElem);
+                for(int i = 0; i < IntMass.Length; i++)
+                Console.Write(IntMass[i] + " ");
+                   
+            }
+                
             
         }
     }
