fda22b0c882c794bc8254adbb3228e16879c3e1e
shohincoding-com <63291350+shohincoding-com@users.noreply.github.com>
2020-04-20 22:25:49 +0500

    Освоил класс ArrayHelp и создал общая структура для методи

---
+++ ArrayHelp.cs
@@ -0,0 +1,25 @@
+using System;
+namespace HW_13_Array
+{
+    public static class ArrayHelp<S>
+    {
+        public static S Pop(ref S[] Mass)
+        {
+            
+        }
+        public static int Push(ref S[] Mass, S NewElem)
+        {
+            
+        }
+
+        public static S Shift(ref S[] Mass)
+        {
+            
+        }
+
+        public static int Unshift(ref S[] Mass, S NewElem)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HW_13_Array;
+
+namespace HW_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                           
+            
+        }
+    }
+}
